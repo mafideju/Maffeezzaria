9e8e9e7f676740bd71259a6a155c196ff293bd81
Marcio Mafideju Rodrigues <mafideju@outlook.com>
2019-11-01 22:29:17 -0300

    feat(boilerplate): create react app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
 
 function App() {
   return (
-    <div className="App">
+    <Fragment>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -19,7 +16,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </Fragment>
   );
 }
 
